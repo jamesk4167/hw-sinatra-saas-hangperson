f1c967b07c27b95e614ee71df69ce8887f1a935a
EC2 Default User <ec2-user@ip-172-31-20-25.us-east-2.compute.internal>
2019-02-08 23:37:22 +0000

    added guess method

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -18,6 +18,17 @@ class HangpersonGame
     @guesses = ""
     @wrong_guesses = ""
   end
+  
+  def guess(guessed_letter)
+    
+    
+    if @word.include?(guessed_letter) 
+      guesses.concat(guessed_letter)
+      return true
+    end
+  
+    
+  end  
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
