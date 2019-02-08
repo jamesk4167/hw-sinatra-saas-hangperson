7ff3149449f006fffece3886f499051660fcf29d
EC2 Default User <ec2-user@ip-172-31-20-25.us-east-2.compute.internal>
2019-02-08 21:21:01 +0000

    did first part of game

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '>= 2.3.1'
+ruby '2.5.3'
 
 gem 'sinatra', '>= 2.0.2'
 gem 'sinatra-flash', '0.3.0'
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,12 +4,19 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  
+  
+  attr_accessor :word, :guesses, :wrong_guesses
+  
+  
 
-  # def initialize()
-  # end
+   def initialize()
+   end
   
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
