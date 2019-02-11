498eaf5506e3792d58b3cdc7069f6a9e41b83398
EC2 Default User <ec2-user@ip-172-31-20-25.us-east-2.compute.internal>
2019-02-11 00:04:34 +0000

    completed cucumber testing

--- app.rb
+++ app.rb
@@ -40,6 +40,16 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    
+    begin
+    result = @game.guess(letter)
+    
+    rescue ArgumentError
+      flash[:message] = "Invalid guess"
+    end
+    if !result.nil? && !result
+      flash[:message] =  "You have already used that letter."
+    end  
     redirect '/show'
   end
   
@@ -50,7 +60,12 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    
+    if @game.check_win_or_lose == :win then redirect '/win'
+    elsif @game.check_win_or_lose == :lose then redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
